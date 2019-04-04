c4571be93e65a9b1b8d727c6a5d9fc6b8c381fb0
Kevin Garvey and Tessa Sullivan <kevin@>
2019-04-04 10:25:18 -0700

    User receives Roman numeral equivalents to the number they enter and errors if they enter anything except a number from 1 to 3999

--- js/scripts.js
+++ js/scripts.js
@@ -39,14 +39,44 @@ function calcTensPlace(tensPlace) {
     return output.join('');
 }
 
+function calcHundredsPlace(hundredsPlace) {
+  output = [];
+  hundred = 'C';
+  fiveHundred = 'D';
+  oneThousand = 'M';
+  for (var i = 1; i<=hundredsPlace; i++) {
+    if (i === 4) {
+      output.splice(1);
+      output.push(fiveHundred);
+    } else if (i === 5){
+      output.shift();
+    } else if ( i=== 9) {
+      output.splice(0,3);
+      output.push(oneThousand);
+    } else {
+      output.push(hundred);
+    }
+  }
+    return output.join('');
+}
+
+function calcThousandsPlace(thousandsPlace) {
+  output = [];
+  thousand = 'M';
+for (var i = 1; i<=thousandsPlace; i++) {
+  output.push(thousand);
+  }
+  return output.join('');
+}
 
 $(document).ready(function() {
   $("#numberForm").submit(function(event) {
     event.preventDefault();
 
     var input = $("#input").val();
-    if ($('input').val() === "") {
-      alert ("Please enter your number");
+
+    if ($('input').val() === "" || parseInt($('input').val()) > 3999 || parseInt($('input').val()) < 1 || isNaN(parseInt($('input').val()))) {
+      alert ("Please enter a number between 1 and 3999.");
     } else {
       var splitNumber = input.split("");
       var onesPlace = splitNumber[splitNumber.length - 1];
@@ -59,15 +89,13 @@ $(document).ready(function() {
       if (splitNumber.length > 3) {
         var thousandsPlace = splitNumber[splitNumber.length - 4];
       }
-      var output = calcTensPlace(tensPlace);
-      console.log(output);
-      // console.log(onesPlace);
-      // console.log(tensPlace);
-      // console.log(hundredsPlace);
-      // console.log(thousandsPlace);
+      var output = calcThousandsPlace(thousandsPlace) + calcHundredsPlace(hundredsPlace) + calcTensPlace(tensPlace) + calcOnesPlace(onesPlace);
+
 
       $("#numberForm").hide();
 
+      $(".input").text(input);
+      $(".output").text(output);
       $("#result").show();
     }
   });
